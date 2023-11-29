f19f0733cf3d80870d854af0cc6ae561d39f17d9
rajkaramchedu <raj@rajkaramchedu.com>
2023-11-28 18:20:00 -0800

    Feedbug widget, one more try

--- static/feedbug-widjet.js
+++ static/feedbug-widjet.js
@@ -1,2 +1,4 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+if (ExecutionEnvironment.canUseDOM) {
 window.onload = ()=>{var s = document.createElement('script'); s.async = "true"; s.defer ="true";s.src="https://feedbug.fly.dev/feedBug.js";window.feedbugApiKey = "6566240de6e2d4155ff2e80a";window.document.head.appendChild(s);}
-  
+}
