3099e716d32623acf8a9e0db27cb25626289bac4
rajkaramchedu <raj@rajkaramchedu.com>
2023-12-05 14:19:33 -0800

    Deleting extraneous docusaurus folder

--- .docusaurus-bak/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/rajkaramchedu/Opentensor/new-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/rajkaramchedu/Opentensor/new-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/rajkaramchedu/Opentensor/new-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/rajkaramchedu/Opentensor/new-docs/src/css/custom.css"),
-];
