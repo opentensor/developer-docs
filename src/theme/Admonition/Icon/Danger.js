e64735a74814525d2fb926d697a2ac594b6e2888
jurgegod <jurgelue65@gmail.com>
2023-12-11 16:50:22 +0600

    Remove Space

--- src/theme/Admonition/Icon/Danger.js
+++ src/theme/Admonition/Icon/Danger.js
@@ -9,8 +9,7 @@ export default function AdmonitionIconDanger(props) {
 	return (
 		<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
 			<path
-					    fillRule="evenodd"
-
+				fillRule="evenodd"
 				d="M11.7334 26.2667L5.7334 20.2667V11.7334L11.7334 5.7334H20.2667L26.2667 11.7334V20.2667L20.2667 26.2667H11.7334ZM12.2001 20.4667L16.0001 16.6667L19.8001 20.4667L20.4667 19.8001L16.6667 16.0001L20.4667 12.2001L19.8001 11.5334L16.0001 15.3334L12.2001 11.5334L11.5334 12.2001L15.3334 16.0001L11.5334 19.8001L12.2001 20.4667Z" />
 		</svg>
 	);
