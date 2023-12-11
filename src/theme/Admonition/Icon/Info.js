aa81253af9d8fe6fb63a851a33a870bf7608fdc4
jurgegod <mailto:jurgelue65@gmail.com>
2023-12-11 18:18:55 +0600

    design updated

--- src/theme/Admonition/Icon/Info.js
+++ src/theme/Admonition/Icon/Info.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 export default function AdmonitionIconInfo(props) {
   return (
     <svg viewBox="0 0 14 16" {...props}>
--- src/theme/Admonition/Icon/Note.js
+++ src/theme/Admonition/Icon/Note.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 export default function AdmonitionIconNote(props) {
   return (
     <svg viewBox="0 0 14 16" {...props}>
