fc27470d9826296f8d50c765848b330a8cf62c8c
jurgegod <jurgelue65@gmail.com>
2023-12-13 18:03:04 +0600

    Fix the search Bar For Mobile Device

--- src/theme/SearchBar/CustomSearchButton.js
+++ src/theme/SearchBar/CustomSearchButton.js
@@ -91,8 +91,15 @@ export default function CustomSearchButton({
 			</div>
 
 			</div>
-			
-
+				<div className="search_mobile_icon" 
+      onClick={onClick}
+      ref={ref}
+      onTouchStart={onTouchStart}
+      onFocus={onFocus}
+      onMouseOver={onMouseOver}>
+					
+			<svg width="20" height="20" class="DocSearch-Search-Icon" viewBox="0 0 20 20"><path d="M14.386 14.386l4.0877 4.0877-4.0877-4.0877c-2.9418 2.9419-7.7115 2.9419-10.6533 0-2.9419-2.9418-2.9419-7.7115 0-10.6533 2.9418-2.9419 7.7115-2.9419 10.6533 0 2.9419 2.9418 2.9419 7.7115 0 10.6533z" stroke="currentColor" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+			</div>
 		</div>
    
     </>
