a80962e3ec3b8bcacb75880e00ba8d10b4bfcb92
rajkaramchedu <raj@rajkaramchedu.com>
2023-12-19 13:30:22 -0800

    Updating social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/bittensor-docs-social-card.png",
       docs: {
         sidebar: {
           autoCollapseCategories: true,
