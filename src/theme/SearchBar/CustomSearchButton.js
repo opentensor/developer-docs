2f9c75529175865fcb3480769cfef184e73dc13c
jurgegod <jurgelue65@gmail.com>
2023-12-20 01:00:28 +0600

    fix: search options

--- src/theme/SearchBar/CustomSearchButton.js
+++ src/theme/SearchBar/CustomSearchButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
-import { IoSearchCircleOutline } from "react-icons/io5";
+// import { IoSearchCircleOutline } from "react-icons/io5";
 
 {
   /* <DocSearchButton
