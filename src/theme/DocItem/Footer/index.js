1be32afddf9d3078a822e56fc46fa75105c46f5f
Onat <53895969+onattech@users.noreply.github.com>
2024-01-20 19:47:33 +0400

    Added vercel feedback actions
    Layed the groundwork but it isn't working yet.

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -2,11 +2,8 @@ import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 import { ThemeClassNames } from "@docusaurus/theme-common";
 import { useDoc } from "@docusaurus/theme-common/internal";
-import LastUpdated from "@theme/LastUpdated";
-import EditThisPage from "@theme/EditThisPage";
 import TagsListInline from "@theme/TagsListInline";
-import styles from "./styles.module.css";
-import { useColorMode } from "@docusaurus/theme-common";
+import { track } from '@vercel/analytics';
 
 function TagsRow(props) {
   return (
@@ -21,51 +18,12 @@ function TagsRow(props) {
     </div>
   );
 }
-function EditMetaRow({
-  editUrl,
-  lastUpdatedAt,
-  lastUpdatedBy,
-  formattedLastUpdatedAt,
-}) {
-  return (
-    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, "row")}>
-      <div className="col">{editUrl && <EditThisPage editUrl={editUrl} />}</div>
 
-      <div className={clsx("col", styles.lastUpdated)}>
-        {(lastUpdatedAt || lastUpdatedBy) && (
-          <LastUpdated
-            lastUpdatedAt={lastUpdatedAt}
-            formattedLastUpdatedAt={formattedLastUpdatedAt}
-            lastUpdatedBy={lastUpdatedBy}
-          />
-        )}
-      </div>
-    </div>
-  );
-}
 export default function DocItemFooter() {
   const { metadata } = useDoc();
-  const {
-    editUrl,
-    lastUpdatedAt,
-    formattedLastUpdatedAt,
-    lastUpdatedBy,
-    tags,
-  } = metadata;
+  const {tags} = metadata;
 
   const canDisplayTagsRow = tags.length > 0;
-  const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
-  const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow;
-  if (!canDisplayFooter) {
-    return null;
-  }
-
-  const { colorMode, setColorMode } = useColorMode();
-  const [status, setStatus] = useState("dark");
-
-  useEffect(() => {
-    setStatus(colorMode);
-  }, [colorMode]);
 
   return (
     <footer
@@ -74,23 +32,15 @@ export default function DocItemFooter() {
         <div className="feedback">
           <span className="feedback_message">was this helpful?</span>
           <div className="feelings">
-            <span className="feeling">:D</span>
-            <span className="feeling">:)</span>
-            <span className="feeling">:(</span>
-            <span className="feeling">x(</span>
+            <span className="feeling" onClick={() => track("VeryHappy")}>:D</span>
+            <span className="feeling" onClick={() => track("Happy")}    >:)</span>
+            <span className="feeling" onClick={() => track("Sad")}      >:(</span>
+            <span className="feeling" onClick={() => track("Signup")}   >x(</span>
           </div>
         </div>
       </div>
 
       {canDisplayTagsRow && <TagsRow tags={tags} />}
-      {/* {canDisplayEditMetaRow && (
-        <EditMetaRow
-          editUrl={editUrl}
-          lastUpdatedAt={lastUpdatedAt}
-          lastUpdatedBy={lastUpdatedBy}
-          formattedLastUpdatedAt={formattedLastUpdatedAt}
-        />
-      )} */}
     </footer>
   );
 }
