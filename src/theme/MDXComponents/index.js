c8a6245378aa096aa28b68b29ba1842388427f4c
Onat <53895969+onattech@users.noreply.github.com>
2024-01-25 11:38:29 +0400

    Added cardSmall component

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -1,10 +1,11 @@
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents"
-import { Cards, Card } from "./Cards/index"
+import { Cards, Card, CardSmall } from "./Cards/index"
 
 export default {
     // Re-use the default mapping
     ...MDXComponents,
     Cards,
     Card,
+    CardSmall,
 }
