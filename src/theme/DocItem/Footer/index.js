357e5791f68e86a689574c6a84512d713084cacd
Onat <53895969+onattech@users.noreply.github.com>
2024-01-29 23:04:28 +0400

    Added optional feedback flag to config file

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -3,7 +3,8 @@ import clsx from "clsx";
 import { ThemeClassNames } from "@docusaurus/theme-common";
 import { useDoc } from "@docusaurus/theme-common/internal";
 import TagsListInline from "@theme/TagsListInline";
-import { track } from '@vercel/analytics';
+import { track } from "@vercel/analytics";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 function TagsRow(props) {
   return (
@@ -21,24 +22,39 @@ function TagsRow(props) {
 
 export default function DocItemFooter() {
   const { metadata } = useDoc();
-  const {tags} = metadata;
+  const { tags } = metadata;
 
   const canDisplayTagsRow = tags.length > 0;
+
+  // Optional feedback
+  const { siteConfig } = useDocusaurusContext();
+  const { customFields } = siteConfig;
+  const { enableFeedback } = customFields;
 
   return (
     <footer
       className={clsx(ThemeClassNames.docs.docFooter, "docusaurus-mt-lg")}>
-      <div className="feedbackAndName">
-        <div className="feedback">
-          <span className="feedback_message">was this helpful?</span>
-          <div className="feelings">
-            <span className="feeling" onClick={() => track("VeryHappy")}>:D</span>
-            <span className="feeling" onClick={() => track("Happy")}>:)</span>
-            <span className="feeling" onClick={() => track("Sad")}>:(</span>
-            <span className="feeling" onClick={() => track("VerySad")}>x(</span>
+      {enableFeedback && (
+        <div className="feedbackAndName">
+          <div className="feedback">
+            <span className="feedback_message">was this helpful?</span>
+            <div className="feelings">
+              <span className="feeling" onClick={() => track("VeryHappy")}>
+                :D
+              </span>
+              <span className="feeling" onClick={() => track("Happy")}>
+                :)
+              </span>
+              <span className="feeling" onClick={() => track("Sad")}>
+                :(
+              </span>
+              <span className="feeling" onClick={() => track("VerySad")}>
+                x(
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
 
       {canDisplayTagsRow && <TagsRow tags={tags} />}
     </footer>
