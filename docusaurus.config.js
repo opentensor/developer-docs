e383108d98e0516100280e22bc6d37e8e9f86908
Raj Karamchedu <rajk@opentensor.dev>
2024-02-07 13:00:41 +0530

    Updated the project name, i.e., git repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "opentensor", // Usually your GitHub org/user name.
-  projectName: "new-docs", // Usually your repo name.
+  projectName: "developer-docs", // Usually your repo name.
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   // Even if you don't use internalization, you can use this field to set useful
