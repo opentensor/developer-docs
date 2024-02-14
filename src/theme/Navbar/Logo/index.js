914d87cd39fae77769b8d13d464c0102c0b63263
Onat <53895969+onattech@users.noreply.github.com>
2024-02-14 15:49:49 +0300

    Update src/theme/Navbar/Logo/index.js
    Co-authored-by: michaτ <96201196+hey-michal@users.noreply.github.com>

--- src/theme/Navbar/Logo/index.js
+++ src/theme/Navbar/Logo/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 export default function NavbarLogo() {
   return (
     <Logo
-      className={'navbar__brand '+ styles.navbar__logo}
+      className={clsx('navbar__brand ', styles.navbar__logo)}
       imageClassName="navbar__logo"
       titleClassName="navbar__title text--truncate"
     />
