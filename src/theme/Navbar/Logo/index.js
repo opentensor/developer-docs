842b6500e8e82ed10bb01dc71247c752820d456b
Onat <53895969+onattech@users.noreply.github.com>
2024-02-14 16:56:21 +0400

    Removed unnecessary class for the logo

--- src/theme/Navbar/Logo/index.js
+++ src/theme/Navbar/Logo/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Logo from '@theme/Logo';
-import styles from './styles.module.css';
 export default function NavbarLogo() {
   return (
     <Logo
-      className={clsx('navbar__brand ', styles.navbar__logo)}
+      className="navbar__brand"
       imageClassName="navbar__logo"
       titleClassName="navbar__title text--truncate"
     />
